ca287cb8a6547acc414c0d344afe8d0c22d9075a
cephalopodsrule <ryan.chien.crv@gmail.com>
2025-04-30 00:28:52 -0400

    Node.h code

---
+++ src/Node.h
@@ -0,0 +1,38 @@
+#pragma once
+
+#include <memory>
+
+using namespace std;
+
+struct Node {
+    char ch;
+    int freq;
+
+    shared_ptr<Node> left;
+    shared_ptr<Node> right;
+
+    Node(char c, int f)
+    {
+        ch = c;
+        freq = f;
+        left = nullptr;
+        right = nullptr;
+    }
+
+    Node(int f, shared_ptr<Node> leftNode, shared_ptr<Node> rightNode)
+    {
+        ch = '\0';
+        freq = f;
+        left = leftNode;
+        right = rightNode;
+    }
+    
+};
+
+struct CompareNodes
+{
+    bool operator()(const shared_ptr<Node>& a, const shared_ptr<Node>& b) const
+    {
+        return (a->freq > b->freq);
+    }
+};
